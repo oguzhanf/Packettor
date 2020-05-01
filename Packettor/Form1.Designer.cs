4850d893cfca09371389fcc7afc14a4a127ccc4c
Oguzhan Filizlibay <filizlibay@hotmail.com>
2020-05-01 12:10:52 +0400

    initial

--- Packettor/Form1.Designer.cs
+++ Packettor/Form1.Designer.cs
@@ -28,21 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNumPacketsReceived = new System.Windows.Forms.Label();
+            this.lblSizeSent = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblNumPacketsSent = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Received";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sent";
+            // 
+            // lblNumPacketsReceived
+            // 
+            this.lblNumPacketsReceived.AutoSize = true;
+            this.lblNumPacketsReceived.Location = new System.Drawing.Point(193, 130);
+            this.lblNumPacketsReceived.Name = "lblNumPacketsReceived";
+            this.lblNumPacketsReceived.Size = new System.Drawing.Size(46, 17);
+            this.lblNumPacketsReceived.TabIndex = 2;
+            this.lblNumPacketsReceived.Text = "label3";
+            // 
+            // lblSizeSent
+            // 
+            this.lblSizeSent.AutoSize = true;
+            this.lblSizeSent.Location = new System.Drawing.Point(327, 147);
+            this.lblSizeSent.Name = "lblSizeSent";
+            this.lblSizeSent.Size = new System.Drawing.Size(46, 17);
+            this.lblSizeSent.TabIndex = 3;
+            this.lblSizeSent.Text = "label4";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(193, 147);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(46, 17);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "label5";
+            // 
+            // lblNumPacketsSent
+            // 
+            this.lblNumPacketsSent.AutoSize = true;
+            this.lblNumPacketsSent.Location = new System.Drawing.Point(327, 130);
+            this.lblNumPacketsSent.Name = "lblNumPacketsSent";
+            this.lblNumPacketsSent.Size = new System.Drawing.Size(46, 17);
+            this.lblNumPacketsSent.TabIndex = 5;
+            this.lblNumPacketsSent.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNumPacketsSent);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblSizeSent);
+            this.Controls.Add(this.lblNumPacketsReceived);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNumPacketsReceived;
+        private System.Windows.Forms.Label lblSizeSent;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblNumPacketsSent;
     }
 }
 
--- Packettor/Form1.cs
+++ Packettor/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Tracing; 
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +9,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Microsoft.Diagnostics.Symbols;
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using Microsoft.Diagnostics.Tracing.Session;
+
 namespace Packettor
 {
     public partial class Form1 : Form
@@ -15,11 +22,55 @@ namespace Packettor
         public Form1()
         {
             InitializeComponent();
+            //if (!(TraceEventSession.IsElevated() == false))
+            //{
+            //    MessageBox.Show("Restart as administrator to run this application.");
+            //    Environment.Exit(2);
+            //}
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            sizeSent = 0;
+            sizeReceived = 0;
+            numSent = 0;
+            numReceived = 0;
+            System.Threading.Thread thr = new System.Threading.Thread(new System.Threading.ThreadStart(startSession));
+            thr.Start();
             
+        }
+        void startSession()
+        {
+            using (var kernelsession = new TraceEventSession(KernelTraceEventParser.KernelSessionName))
+            {
+                kernelsession.EnableKernelProvider(KernelTraceEventParser.Keywords.NetworkTCPIP);
+                kernelsession.Source.Kernel.TcpIpSend += Kernel_TcpIpSend;
+                kernelsession.Source.Kernel.TcpIpRecv += Kernel_TcpIpRecv;
+                
+                kernelsession.Source.Process();
+            }
+        }
+        int sizeSent, sizeReceived, numSent, numReceived;
+        private void Kernel_TcpIpRecv(TcpIpTraceData obj)
+        {
+            sizeReceived += obj.size;
+            ++numReceived;
+            Invoke(new dlgUpdateui(updateui));
+        }
+
+        private void Kernel_TcpIpSend(TcpIpSendTraceData obj)
+        {
+            sizeSent += obj.size;
+            ++numSent;
+            Invoke(new dlgUpdateui(updateui));
+        }
+        delegate void dlgUpdateui();
+        void updateui()
+        {
+            lblNumPacketsReceived.Text = numReceived.ToString();
+            lblSize.Text = sizeReceived.ToString();
+            lblNumPacketsSent.Text = numSent.ToString();
+            lblSizeSent.Text = sizeSent.ToString();
         }
     }
 }
