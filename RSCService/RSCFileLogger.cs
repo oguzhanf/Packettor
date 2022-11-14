6078077e8d8d4c3c0b99e7dccab3f438c38c5c64
oguzhanf <oguzhanf@TELEVIZYON>
2022-11-14 08:43:17 +0400

    minor changes to log file things

--- RSCService/RSCFileLogger.cs
+++ RSCService/RSCFileLogger.cs
@@ -12,32 +12,21 @@ namespace RSCService
 {
     public partial class RSCService : ServiceBase
     {
-        //FileLogTraceListener LogTraceListener;
+        
         StreamWriter sw;
         private void prepLogFile() 
         { 
-            //LogTraceListener = new FileLogTraceListener();
-            //LogTraceListener.LogFileCreationSchedule = LogFileCreationScheduleOption.Daily;
-            //LogTraceListener.MaxFileSize = 1000000;
-            //LogTraceListener.Location = LogFileLocation.Custom;
-            //LogTraceListener.CustomLocation = logFileLocation;
-            //LogTraceListener.DiskSpaceExhaustedBehavior = DiskSpaceExhaustedOption.DiscardMessages;
-            //LogTraceListener.AutoFlush = true;  
-            //LogTraceListener.IncludeHostName = true;    
-            //LogTraceListener.Append = true;
-            sw = new StreamWriter(logFileNameWPath);
+            sw = new StreamWriter(logFileNameWPath, true);
             sw.AutoFlush= true;
             
         }
         int fileCursor = 0;
-        int maxCursor = 1000000;
+        int maxCursor = 1024 * 1024 * 1024;
         private void LogToFile(string message)
         {
-            //sw.BaseStream.Seek(fileCursor+256, SeekOrigin.Begin);
-            //sw.WriteLine(message);
             if (fileCursor > maxCursor) { fileCursor = 0; }
             sw.BaseStream.Seek(fileCursor, SeekOrigin.Begin);
-            message += "\r\n";
+            message += "::" + fileCursor.ToString() + "\r\n";
             sw.Write(message, 0, message.Length);
             fileCursor += message.Length ;
         }
